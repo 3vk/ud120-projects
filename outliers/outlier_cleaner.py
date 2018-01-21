a839ef4ed66af36da7e03b69efdd4d635d6c1af1
TRIVIKRAM CHALASANI <3vkchalasani95@gmail.com>
2018-01-21 16:21:59 -0600

    Clean away the outliers

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-
+from operator import itemgetter
+from operator import attrgetter
 def outlierCleaner(predictions, ages, net_worths):
     """
         Clean away the 10% of points that have the largest
@@ -14,6 +15,13 @@ def outlierCleaner(predictions, ages, net_worths):
     cleaned_data = []
 
     ### your code goes here
+    error = (net_worths-predictions)** 2
+    zipped = zip(ages,net_worths,error)
+    zipped.sort(key=itemgetter(2), reverse = True)
+    #zipped.sort(key=attrgetter('error'), reverse = True)
+    #zipped.sort(key=lambda x: x[2], reverse = True)
+    k= int(len(ages)*0.1)
+    cleaned_data = zipped[k:]
 
     
     return cleaned_data
